9baaf21da138fae6801510db76d1fc8db457491f
Nirmal Seneviratne <nirmal.seneviratne@gmail.com>
2025-07-21 20:56:57 +1200

    Allow all origins.

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4000',
-    credentials: true,
+    origin: '*',
   });
   await app.listen(3000);
 }
