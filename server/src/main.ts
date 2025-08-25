a9706266d2983cfd4702316c62adc2f556307afe
Nirmal Seneviratne <nirmal.seneviratne@gmail.com>
2025-08-25 19:27:20 +1200

    feat: integrate serverless framework and AWS Lambda support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://cineverse-frontend-sen.s3-website-ap-southeast-2.amazonaws.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end(); // Respond to preflight
+    }
+    next();
   });
-  await app.listen(3000);
+
+  await app.init();
+  
+  
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+
